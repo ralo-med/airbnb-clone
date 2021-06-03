06638abc1146429181bb4c4aa8021538aee7745e
Ralo <74175604+ralo-med@users.noreply.github.com>
2021-06-03 14:26:12 +0900

    #11.8

--- rooms/views.py
+++ rooms/views.py
@@ -1,5 +1,7 @@
+from django.utils import timezone
 from django.views.generic import ListView
 from . import models
+
 
 # Create your views here.
 class HomeView(ListView):
@@ -10,3 +12,10 @@ class HomeView(ListView):
     paginate_by = 10
     paginate_orphans = 5
     ordering = "created"
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context
