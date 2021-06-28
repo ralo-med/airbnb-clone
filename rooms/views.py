84c4d59d7c438dd79b2703cd680e35c4a33569d6
Ralo <74175604+ralo-med@users.noreply.github.com>
2021-06-29 06:14:53 +0900

    #13.2

--- rooms/views.py
+++ rooms/views.py
@@ -43,9 +43,27 @@ def search(request):
     print(request.GET)
     city = request.GET.get("city", "Anywhere")
     city = str.capitalize(city)
+    country = request.GET.get("country", "KR")
+    room_type = int(
+        request.GET.get(
+            "room_type",
+        )
+    )
     room_types = models.RoomType.objects.all()
+
+    form = {
+        "city": city,
+        "s_room_type": room_type,
+        "s_country": country,
+    }
+
+    choices = {
+        "countries": countries,
+        "room_types": room_types,
+    }
+
     return render(
         request,
         "rooms/search.html",
-        {"city": city, "countries": countries, "room_types": room_types},
+        {**form, **choices},
     )
