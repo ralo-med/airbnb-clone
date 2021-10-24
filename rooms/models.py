00657f0db45006d4bed43ab790d9aefd95f2d14e
Ralo <74175604+ralo-med@users.noreply.github.com>
2021-10-25 01:07:00 +0900

    #24.3 Finishing Calendar

--- rooms/models.py
+++ rooms/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.urls import reverse
 from django_countries.fields import CountryField
 from core import models as core_models
+from cal import Calendar
+import datetime
+from dateutil import relativedelta
 
 
 class AbstractItem(core_models.TimeStampedModel):
@@ -122,10 +125,9 @@ class Room(core_models.TimeStampedModel):
         photos = self.photos.all()[1:5]
         return photos
 
-
-def become_host(request):
-    pass
-
-
-def become_quest(request):
-    pass
+    def get_calendars(self):
+        today = datetime.date.today()
+        nextmonth = today + relativedelta.relativedelta(months=1)
+        this_month = Calendar(today.year, today.month)
+        next_month = Calendar(nextmonth.year, nextmonth.month)
+        return [this_month, next_month]
